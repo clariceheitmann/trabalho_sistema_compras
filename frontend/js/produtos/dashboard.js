b21ec85ebe9472bb5cefeb99c1b26e02acb6bad7
Clarice <clarice_h_santos@estudante.sesisenai.org>
2026-08-04 19:36:22 -0300

    fazendo dashboard

---
+++ frontend/js/produtos/dashboard.js
@@ -0,0 +1,66 @@
+const container = document.getElementById('container_produtos');
+
+console.log(container);
+
+
+fetch('http://localhost:3000/produtos')
+
+.then(res => res.json())
+
+.then(dados => {
+
+
+    container.innerHTML = "";
+
+
+    dados.forEach(produto => {
+
+
+        container.innerHTML += `
+
+            <div class="card">
+
+
+                <img 
+                src="${produto.imagem}" 
+                alt="${produto.nome}"
+                >
+
+
+                <h3>${produto.nome}</h3>
+
+
+                <p>${produto.categoria}</p>
+
+
+                <p class="preco">
+                    R$ ${produto.preco}
+                </p>
+
+
+                <p class="estoque">
+                    Estoque: ${produto.qtdeEstoque}
+                </p>
+
+
+            </div>
+
+        `;
+
+
+    });
+
+
+})
+
+
+.catch(err => {
+
+
+    console.error(
+        "Erro ao buscar produtos:",
+        err
+    );
+
+
+});
